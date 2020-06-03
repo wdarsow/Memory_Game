5146b316a399b9bae774dc4f7c8bc5e843640ad7
Wade <wade.darsow@gmail.com>
2020-06-02 20:17:48 -0700

    initial scaffolding

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import "../styles/Header.css";
+
+const Header = () => (
+    <h1 className="header">Memory Game</h1>
+)
+
+export default Header;
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "../styles/Navbar.css";
+
+const Navbar = () => (
+    <div className="navbarDiv">
+        <h1 className="mainHeader">Memory Game!</h1>
+        <h3 className="secondaryHeader">Click on an image to earn points but don't click on any images more than once!</h3>
+    </div>
+)
+
+export default Navbar;
