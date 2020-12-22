77c56e0e71eb8323c328601c0de0c8a8621b7423
Wade <wade.darsow@gmail.com>
2020-12-21 19:08:16 -0800

    Updates

---
+++ precache-manifest.b45dc621084406950b0e29bceaef0f62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b65676a3e41a77f6e50844535b9448d3",
+    "url": "/wdarsow/memory_game/index.html"
+  },
+  {
+    "revision": "172a7d58c121ecb5d927",
+    "url": "/wdarsow/memory_game/static/css/main.d548e43d.chunk.css"
+  },
+  {
+    "revision": "6a5e29e9c417ecb789af",
+    "url": "/wdarsow/memory_game/static/js/2.f98e5288.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wdarsow/memory_game/static/js/2.f98e5288.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "172a7d58c121ecb5d927",
+    "url": "/wdarsow/memory_game/static/js/main.075b56a3.chunk.js"
+  },
+  {
+    "revision": "01f24c24b37858aac8c2",
+    "url": "/wdarsow/memory_game/static/js/runtime-main.6bf99b8f.js"
+  }
+]);
